b3ae6c6faf2bfde34873d7b1ad14fd3d0c7b4087
Christopher Marim <cssmarim1@gmail.com>
2021-08-24 22:34:08 -0300

    Mudança post Coleta

--- src/components/Collect.js
+++ src/components/Collect.js
@@ -1,83 +1,101 @@
-import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, Alert, Vibration } from "react-native";
-import moment from "moment";
-import "moment/locale/pt-br";
-import {useDispatch } from "react-redux";
-import Swipeable from "react-native-gesture-handler/Swipeable";
-import commonStyles from "../commonStyles";
-import Icon from "react-native-vector-icons/FontAwesome";
-import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Alert,
+  Vibration,
+} from 'react-native';
+import moment from 'moment';
+import 'moment/locale/pt-br';
+import {useDispatch, useSelector} from 'react-redux';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+import commonStyles from '../commonStyles';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-
-import getRealm from "../services/realm";
+import getRealm from '../services/realm';
+import { API } from '../../commonsVariables';
 
 export default function Collect(props) {
-  const [borderRadiusCONST, setborderRadius] = useState(10);  
+  const [borderRadiusCONST, setborderRadius] = useState(10);
   const [collects, setCollects] = useState([]);
+  const [user, setUser] = useState();
   const [BaseURL, setBaseURL] = useState('');
   const [HeaderValue, setHeaderValue] = useState('');
   const dispatch = useDispatch();
 
+  async function getUsuarioRealm() {
+    const realm = await getRealm();
+    const store = realm.objects('User');
+    setUser(store[0]);
+  }
+
+  useEffect(() => {
+    getUsuarioRealm();
+  }, []);
+
   const formatteddate = (collects) =>
-    moment(collects.dateAt).locale("pt-br").format("D/MM/YYYY");
-    
-    const getData = async () => {
-      try {
-        const apiText = await AsyncStorage.getItem('@API')
-        const apiValue = await AsyncStorage.getItem('@Value')
-       
-        if(apiText !== null && apiValue !== null) {
-          console.warn(HeaderValue)
-          setBaseURL(apiText)
-          setHeaderValue(apiValue)
-        }
-      } catch(e) {
-        // error reading value
+    moment(collects.dateAt).locale('pt-br').format('D/MM/YYYY');
+
+  const getData = async () => {
+    try {
+      const apiText = await AsyncStorage.getItem('@API');
+      const apiValue = await AsyncStorage.getItem('@Value');
+
+      if (apiText !== null && apiValue !== null) {
+        setBaseURL(apiText);
+        setHeaderValue(apiValue);
       }
+    } catch (e) {
+      alert(e);
     }
-    const api = axios.create({
-      baseURL:`${BaseURL}`,
-      headers:{Authorization:`${HeaderValue}`}
-  })
+  };
+  const api = axios.create({
+    baseURL: BaseURL ? BaseURL : API.BASE_URL_API,
+    headers: {
+      Authorization: HeaderValue
+        ? HeaderValue
+        : API.HEADER_AUTHORIZATION_API,
+    },
+  });
 
   async function setApi() {
+    console.log(user);
     try {
-      const response = await api.get("/ColetaElemento");
-      collects.itens.forEach(
-        (x) => {
-          api.post("/ColetaElemento", {
-            ColetaId: x.numberCollect,
-            ElementoId: x.element,
-            Valor: x.value,
-          });
-        }
-      );
-      
-        Vibration.vibrate(200)
-      console.log(response.data);
-      DelCollect()
-      Alert.alert("Lote Enviado", `Lote ${props.nome} enviado com sucesso`);
+      collects.itens.forEach((x) => {
+        api.post('/ColetaElemento', {
+          coletaid: x.numberCollect,
+          elementoid: x.element,
+          valor: x.value,
+          system_unit_id: user.system_unit_id,
+          system_user_id: user.system_user_id,
+        });
+      });
+
+      Vibration.vibrate(200);
+      DelCollect();
+      Alert.alert('Lote Enviado', `Lote ${props.nome} enviado com sucesso`);
     } catch (error) {
-      console.log("deu erro " + error);
-      Alert.alert("Post não concluido",`Verificar informações da Api em configurações`)
+      Alert.alert(
+        'Post não concluido',
+        `Verificar informações da Api em configurações, error: ${error}`,
+      );
     }
   }
-  async function loadCollects(){
+  async function loadCollects() {
     const realm = await getRealm();
     let idCollect = props.id;
-    let data = realm.objectForPrimaryKey("Collects",idCollect)
-  setCollects(data)
-}
+    let data = realm.objectForPrimaryKey('Collects', idCollect);
+    setCollects(data);
+  }
 
-  useEffect(()=>{
-    
-    getData()
-    
-
-    loadCollects()
-
-  }, [])
+  useEffect(() => {
+    getData();
+    loadCollects();
+  }, []);
 
   const getLeftContent = () => {
     return (
@@ -86,43 +104,41 @@ export default function Collect(props) {
           style={styles.left1}
           activeOpacity={0.5}
           onPress={() => {
+            getUsuarioRealm();
             setApi();
-          }}
-        >
+          }}>
           <Icon name="send" size={20} color="white" />
         </TouchableOpacity>
         <TouchableOpacity
           onPress={() => {
-            dispatch({ type: "SHOW_MODAL_EDTCOLLECT_ON" });
-            dispatch({ type: "CURRENT_ID", payload: [props.id] });
-            console.log(props.id)
+            dispatch({type: 'SHOW_MODAL_EDTCOLLECT_ON'});
+            dispatch({type: 'CURRENT_ID', payload: [props.id]});
+            console.log(props.id);
           }}
           style={styles.left2}
-          activeOpacity={0.5}
-        >
+          activeOpacity={0.5}>
           <Icon name="pencil" size={20} color="white" />
         </TouchableOpacity>
       </View>
     );
   };
 
-  function refresh(){
-    dispatch({ type: "REFRESH", payload: [true] });
+  function refresh() {
+    dispatch({type: 'REFRESH', payload: [true]});
     setInterval(() => {
-      dispatch({ type: "REFRESH", payload: [false] });
+      dispatch({type: 'REFRESH', payload: [false]});
     }, 1000);
   }
-  
+
   async function DelCollect() {
     const realm = await getRealm();
     let idCollect = props.id;
-    let object = realm.objectForPrimaryKey("Collects",idCollect)
+    let object = realm.objectForPrimaryKey('Collects', idCollect);
 
     realm.write(() => {
       realm.delete(object);
     });
-    refresh()
-    
+    refresh();
   }
 
   const getRightContent = () => {
@@ -133,8 +149,7 @@ export default function Collect(props) {
             DelCollect();
           }}
           style={styles.right}
-          activeOpacity={0.5}
-        >
+          activeOpacity={0.5}>
           <Icon name="trash" size={20} color="white" />
         </TouchableOpacity>
       </View>
@@ -146,8 +161,7 @@ export default function Collect(props) {
       renderLeftActions={getLeftContent}
       renderRightActions={getRightContent}
       onSwipeableWillOpen={() => setborderRadius(0)}
-      onSwipeableWillClose={() => setborderRadius(10)}
-    >
+      onSwipeableWillClose={() => setborderRadius(10)}>
       <View
         style={[
           styles.container,
@@ -155,24 +169,22 @@ export default function Collect(props) {
             borderBottomLeftRadius: borderRadiusCONST,
             borderTopLeftRadius: borderRadiusCONST,
           },
-        ]}
-      >
+        ]}>
         <TouchableOpacity
           style={styles.button}
           onPress={() => {
-            props.navigation.navigate("ItemList");
-            dispatch({ type: "CURRENT_ID", payload: [props.id] });
-          }}
-        >
-          <View style={{ alignItems: "center", flexDirection: "row" }}>
+            props.navigation.navigate('ItemList');
+            dispatch({type: 'CURRENT_ID', payload: [props.id]});
+          }}>
+          <View style={{alignItems: 'center', flexDirection: 'row'}}>
             <View style={styles.textCollect}>
               <Text style={styles.nomeCollect}>{props.nome}</Text>
-              <Text style={{ fontWeight: commonStyles.fontWeight }}>
+              <Text style={{fontWeight: commonStyles.fontWeight}}>
                 Data: {formatteddate(`${props.dateAt}`)}
               </Text>
             </View>
-            <View style={{ justifyContent: "center", padding: 8 }}>
-              <Text style={{ fontWeight: commonStyles.fontWeight }}>
+            <View style={{justifyContent: 'center', padding: 8}}>
+              <Text style={{fontWeight: commonStyles.fontWeight}}>
                 Coletas: {props.itens.length}
               </Text>
             </View>
@@ -186,21 +198,21 @@ export default function Collect(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "row",
+    flexDirection: 'row',
     elevation: 1,
     borderLeftWidth: 5,
-    alignItems: "center",
-    flexWrap: "wrap",
+    alignItems: 'center',
+    flexWrap: 'wrap',
     paddingVertical: 10,
     width: '100%',
     borderRadius: 5,
     borderLeftColor: commonStyles.color.principal,
-    backgroundColor: "white",
+    backgroundColor: 'white',
   },
   textCollect: {
     flex: 1,
-    alignItems: "flex-start",
-    justifyContent: "space-between",
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
     padding: 10,
   },
   nomeCollect: {
@@ -210,32 +222,32 @@ const styles = StyleSheet.create({
   },
   button: {
     flex: 1,
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
   },
   left1: {
-    backgroundColor: "#194c9e",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#194c9e',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingHorizontal: 20,
     borderBottomLeftRadius: 10,
     borderTopLeftRadius: 10,
   },
   left2: {
-    backgroundColor: "#4287f5",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#4287f5',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingHorizontal: 20,
   },
   containerSwipeable: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   right: {
-    backgroundColor: "#bf1f1f",
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#bf1f1f',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     paddingHorizontal: 20,
     borderRadius: 0,
     borderBottomRightRadius: 10,
